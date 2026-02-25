d7927bf9399d0b7ca1bae72c754c8b77cd2b8840
francescocalabrese2005-spec <francesco.calabrese.2005@gmail.com>
2026-02-25 19:56:36 +0100

    category done

--- models/category.py
+++ models/category.py
@@ -1,2 +1,12 @@
 class Category:
-    ...
+    __id: int
+    name: str
+
+    @property
+    def id(self):
+        return self.__id
+
+
+    def __init__(self, id: int, name: str):
+        self.__id = id
+        self.name = name
