17a5d1195056373c40258f970732fe3cf12f39d6
francescocalabrese2005-spec <francesco.calabrese.2005@gmail.com>
2026-02-25 20:06:25 +0100

    creata classe Expense

---
+++ models/expense.py
@@ -0,0 +1,25 @@
+from category import Category
+
+class Expense: 
+    
+    __id : int
+    value : list[int]
+    __category : Category
+    data : str
+    
+    
+    @property
+    def id(self):
+        return self.__id
+    
+    
+    @property
+    def category(self):
+        return self.__category
+    
+    
+    def __init__(self, id : int, value : list[int], category : Category, data : str) :
+        self.__id = id
+        self.value = value
+        self.__category = category
+        self.data : str
