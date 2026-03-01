e0d5794fb6c8859cbfea1cd39539ec024f4b16c2
francescocalabrese2005-spec <francesco.calabrese.2005@gmail.com>
2026-03-01 18:00:47 +0100

    aggiunte funzioni scrivi, modifica, crea_spesa e setter di category

--- models/expense.py
+++ models/expense.py
@@ -12,11 +12,15 @@ class Expense:
     def id(self):
         return self.__id
     
-    
     @property
     def category(self):
         return self.__category
     
+    @property.setter
+    def category(self, new_category : Category):
+        if isinstance(new_category, Category):
+            self.__category = new_category
+            
     
     def __init__(self, id : int, value : list[int], category : Category, data : str) :
         self.__id = id
--- services/finance_service.py
+++ services/finance_service.py
@@ -1,13 +1,31 @@
 from database.json_storage import ExpenseStorage
+from models.expense import Expense
 class FinanceService:
     __db: ExpenseStorage
-
+    
     @property
     def db(self):
         return self.__db
     
     def __init__(self,db:ExpenseStorage):
         self.__db=db
+        
+    def crea_spesa(self):
+        new_spesa= {
+            "id" : self.db.expenses[-1].id + 1,
+            "value" : [input(f"inserisci valore {i} " for i in range(1,3))],
+            "catgoryname" : input("è una spesa di alimentari o videogiochi? "),
+            "data" : input("inserisci data di acquisto")
+            
+        }
+        if new_spesa["catgoryname"].strip().lower() == "alimentari":
+            new_spesa["catgoryname"]= 1
+        elif new_spesa["catgoryname"].strip().lower() == "videogiochi":
+            new_spesa["catgoryname"]= 2
+        else:
+            raise ValueError("sei un coglione!")
+        return new_spesa
+    
     
     def leggi(self):
         self.db.apri()
@@ -20,11 +38,30 @@ class FinanceService:
 
 
 
-    def modifica(self):
-        ...
+    def modifica(self, valori : list[int], categoria : int, id : int):
+        self.db.apri()
+        for spese in self.db.file["expenses"]:
+            if id == spese.id:
+                spese.value= valori
+                for categorie in self.db.file["categories"]:
+                    if categoria == categorie.id:
+                        spese.category= categorie
+                        self.db.chiudi()
+                        return True
+        self.db.chiudi()
+        return False
+    
     
     def scrivi(self):
-        ...
+        self.db.apri()
+        new_spesa= self.crea_spesa()
+        for categorie in self.db.file["categories"]:
+            if new_spesa["categoryname"] == categorie.id:
+                new_spesa["categoryname"]= categorie
+                crea= Expense(new_spesa["id"], new_spesa["value"], new_spesa["categoryname"], new_spesa["data"])
+                self.db.file["expenses"].append(crea)
+        self.db.chiudi()
+    
     
     def elimina(self):
         ...
