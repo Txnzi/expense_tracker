37454cc03ecbf9cbbe602a4285014608a0368aa3
francescocalabrese2005-spec <francesco.calabrese.2005@gmail.com>
2026-03-01 18:23:09 +0100

    concluso file finance_service

--- services/finance_service.py
+++ services/finance_service.py
@@ -2,17 +2,24 @@ from database.json_storage import ExpenseStorage
 from models.expense import Expense
 class FinanceService:
     __db: ExpenseStorage
+    id_liberati : None
     
     @property
     def db(self):
         return self.__db
     
-    def __init__(self,db:ExpenseStorage):
+    def __init__(self,db:ExpenseStorage, id_liberati : None):
         self.__db=db
+        self.id_liberati= [int] | None
         
     def crea_spesa(self):
+        if self.id_liberati is not None:
+            nuovo_id= self.id_liberati[0]
+            self.id_liberati= self.id_liberati[1:]
+        else:
+            nuovo_id= self.db.expenses[-1].id + 1
         new_spesa= {
-            "id" : self.db.expenses[-1].id + 1,
+            "id" : nuovo_id,
             "value" : [input(f"inserisci valore {i} " for i in range(1,3))],
             "catgoryname" : input("è una spesa di alimentari o videogiochi? "),
             "data" : input("inserisci data di acquisto")
@@ -30,11 +37,9 @@ class FinanceService:
     def leggi(self):
         self.db.apri()
         lista = [{"id": el.id} for el in self.db.file["expenses"]]
-
-
-
-
         self.db.chiudi()
+        
+        return lista
 
 
 
@@ -63,11 +68,22 @@ class FinanceService:
         self.db.chiudi()
     
     
-    def elimina(self):
-        ...
+    def elimina(self, id : int):
+        self.db.apri()
+        for spesa in self.db.file["expenses"]:
+            if id == spesa.id:
+                self.id_liberati.append(id)
+                del spesa
+        self.db.chiudi()
 
-    def leggi_dett(self):
-        ...
+
+    def leggi_dett(self, id : int):
+        self.db.apri()
+        for spesa in self.db.file["expenses"]:
+            if spesa.id == id:
+                spesa_dettagliata={"id" : id, "value" : spesa.value, "categoryId" : spesa.category, "data" : spesa.data}
+                return spesa_dettagliata
+        return False
 
     
 
